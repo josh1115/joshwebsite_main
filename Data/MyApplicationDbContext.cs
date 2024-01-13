aac9649367d4fc913a80eaffbd96167213e92b62
josh1115 <jmhorn1115@gmail.com>
2024-01-12 21:06:59 -0600

    Add project files.

---
+++ Data/MyApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace joshwebsite_main.Data
+
+{
+    public class MyApplicationDbContext : DbContext
+    {
+        public MyApplicationDbContext(DbContextOptions<MyApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+
+
+
+
+
+
+    }
+}
